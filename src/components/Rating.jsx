e380ecbeb9b857ab581885f6d9f3fb5cd7ca7224
Daris Bouaziz <mkdarice@MacBookAir-de-Daris.local>
2026-01-26 13:51:11 -0500

    Submit Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -32,8 +32,21 @@ const Rating = ({
 }) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
 
   const stars = Array.from({ length: 5 }, (_, i) => i + 1); //Create [1, 2, 3, 4, 5]
+
+  const handleSubmit = () => {
+    if (rating > 0) {
+      setSubmitted(true);
+    }
+  };
+
+  const closeModel = () => {
+    setSubmitted(false);
+    setRating(0);
+    setHover(0);
+  };
 
   return (
     <div className="rating-container">
@@ -53,6 +66,28 @@ const Rating = ({
         ))}
       </div>
       {rating > 0 && <p className="feedback">{feedbackMessages[rating - 1]}</p>}
+
+      <button
+        className="submit-btn"
+        onClick={handleSubmit}
+        disabled={rating === 0}
+      >
+        Submit
+      </button>
+      {/* Modal */}
+      {submitted && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <h2>Thank You</h2>
+            <p>
+              You rated us {rating} star{rating > 1 ? "s" : ""}
+            </p>
+            <button className="close-btn" onClick={closeModel}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
